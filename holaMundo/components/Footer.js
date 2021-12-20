e6bceb5c34985bc43f3dd1be2a01305649becc01
jaumebt90 <jaume.mx90@gmail.com>
2021-12-20 16:04:34 +0100

    Update Footer.js

--- holaMundo/components/Footer.js
+++ holaMundo/components/Footer.js
@@ -1,7 +1,13 @@
 app.component("footer-banco", {
   template: /*html*/ `
   <div class="bg-dark py-3 mt-2 text-white">
-        <h3>Footer de mi sitio web</h3>
+        <h3>{{texto}} - {{cantidad}}</h3>
   </div>
   `,
+  data() {
+    return {
+      texto: "Footer del sitio web dinamico",
+      cantidad: 1000,
+    };
+  },
 });
