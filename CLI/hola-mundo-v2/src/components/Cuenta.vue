99b6b697259bb66052133848f30975791d0ec219
jaumebt90 <jaume.mx90@gmail.com>
2021-12-20 17:23:17 +0100

    Update Cuenta.vue

--- CLI/hola-mundo-v2/src/components/Cuenta.vue
+++ CLI/hola-mundo-v2/src/components/Cuenta.vue
@@ -2,6 +2,9 @@
   <h2>Tipo de cuenta: {{ cuenta }}</h2>
   <h2>Saldo: {{ saldo }}</h2>
   <h2>Cuenta {{ estado ? "Activa" : "Inactiva" }}</h2>
+  <div v-for="(servicio, index) in servicios" :key="index">
+    {{ index + 1 }} - {{ servicio }}
+  </div>
 </template>
 
 <script>
